af5da50734c2f9890c602af8ef4d711b23ec45a1
Keicabba <peppino_cbr@hotmail.com>
2023-01-28 04:22:46 +0100

    Initial commit

---
+++ src/it/figuccia/House.java
@@ -0,0 +1,12 @@
+package it.figuccia;
+
+public class House {
+    // instance variables
+    public String address;
+    public int numberOfFloors;
+
+    // constructor method
+    public House() {
+        System.out.println("New House added!");
+    }
+}
---
+++ src/it/figuccia/Tester.java
@@ -0,0 +1,13 @@
+package it.figuccia;
+
+public class Tester {
+    public static void main(String[] args) {
+        //declaring two House variables
+        House house1;
+        House house2;
+
+        //creating two House objects
+        house1 = new House();
+        house2 = new House();
+    }
+}
